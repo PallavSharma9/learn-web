87f342f69c9b9d80ccaf363d3e887edabd4908b5
PallavSharma9 <pallavsharma0202@gmail.com>
2026-02-02 20:36:28 +0530

    adding full courses component

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,30 +1,69 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import courseImage from "@/images/course-image.png";
+const Courses = [
+  {
+    picture: courseImage,
+    title: "Learn-Web Combined Bootcamp",
+    description:
+      "Web dev (Every Friday) Devops (Every Friday) Machine Learning and AI (Every Saturday) Web3 (Every Sunday)",
+    price: "$160",
+    discount: "34",
+  },
+  {
+    picture: courseImage,
+    title: "Web Dev + Devops Bootcamp",
+    description: "Web dev Devops",
+    price: "$160",
+    discount: "34",
+  },
+  {
+    picture: courseImage,
+    title: "Web3 Bootcamp",
+    description: "Complete Solana and Web3 Bootcamp",
+    price: "$160",
+    discount: "34",
+  },
+  {
+    picture: courseImage,
+    title: "AI and ML Bootcamp",
+    description: "AI and ML Syllabus",
+    price: "$160",
+    discount: "34",
+  },
+  {
+    picture: courseImage,
+    title: "Learn-Web Combined Bootcamp",
+    description:
+      "Web dev (Every Friday) Devops (Every Friday) Machine Learning and AI (Every Saturday) Web3 (Every Sunday)",
+    price: "$160",
+    discount: "34",
+  },
+];
+</script>
 
 <template>
-  <div class="py-20 bg-black px-4">
+  <div class="py-20 bg-black px-4 lg:px-20">
     <h1
       class="text-white text-center font-semibold text-3xl mb-12 md:text-4xl lg:text-5xl"
     >
       Featured Cohorts
     </h1>
-    <div class="flex flex-column md:flex-row gap-4 flex-wrap">
+    <div class="flex flex-column md:flex-row justify-center gap-4 flex-wrap">
       <!-- course card  -->
-      <div class="p-4 bg-white rounded-2xl max-w-114">
-        <img
-          class="rounded-xl max-w-105"
-          src="../images/course-image.png"
-          alt=""
-        />
+      <div
+        v-for="course in Courses"
+        class="container p-4 bg-white rounded-2xl max-w-100"
+      >
+        <img class="rounded-xl" :src="course.picture" alt="" />
         <h3 class="mt-4 mb-8 text-2xl font-medium">
-          Learn-Web Combined Bootcamp
+          {{ course.title }}
         </h3>
         <p class="text-zinc-600 mb-6">
-          Web dev (Every Friday) Devops (Every Friday) Machine Learning and AI
-          (Every Saturday) Web3(Every Sunday)...
+          {{ course.description }}
         </p>
         <div class="flex justify-between mb-10">
           <h2 class="text-3xl font-bold">
-            $139 <span class="text-zinc-400">$160 </span>
+            $139 <span class="text-zinc-400">{{ course.price }} </span>
           </h2>
           <div class="rounded-full bg-green-100 text-green-600 px-3 py-2">
             34% off
