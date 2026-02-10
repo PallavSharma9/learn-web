45948f3684ceb0fbb1e7d24bcf46db33ad4e1354
PallavSharma9 <pallavsharma0202@gmail.com>
2026-02-10 09:26:42 +0530

    adding NotFoundView

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import CoursesView from "@/views/CoursesView.vue";
 import CourseDetailsView from "@/views/CourseDetailsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: "course",
       component: CourseDetailsView,
       props: true,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
